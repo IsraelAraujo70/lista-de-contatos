79fd5c9ab4dffb202ea0f27ad8994636c90302e4
IsraelAraujo70 <israelaraujodeoliveira@gmail.com>
2024-12-06 15:32:06 -0300

    fix: downgrade to react-18 and next-14 to solve peer dependency issue

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  compiler: {
+    styledComponents: true,
+  },
+  reactStrictMode: true,
+}
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
+module.exports = nextConfig
